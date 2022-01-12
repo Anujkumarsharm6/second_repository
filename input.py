21fdb25466fbd4d1f455ded7d918e7d934cc7617
Anujkumarsharm6 <anuj.k.sharma@hotmail.com>
2022-01-12 21:21:06 +0530

    user input

---
+++ input.py
@@ -0,0 +1,5 @@
+name = input("what is your name?")
+age =  input("what is your age?")
+address = input("where do you live?")
+
+print("Hello!" +name+" your age is " +age+ " and you live in " +address)
