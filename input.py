a902ab7be7003b25bbbe189c8cd98e20ff2ba93a
Anujkumarsharm6 <92105677+Anujkumarsharm6@users.noreply.github.com>
2022-01-12 21:26:20 +0530

    food added

--- input.py
+++ input.py
@@ -1,5 +1,7 @@
 name = input("what is your name?")
 age =  input("what is your age?")
 address = input("where do you live?")
+food = input("what do you like to have?")
 
-print("Hello!" +name+" your age is " +age+ " and you live in " +address)
+
+print("Hello!" +name+" your age is " +age+ " and you live in " +address+"I would like to have" + food)
