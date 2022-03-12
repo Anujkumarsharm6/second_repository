77124bbb322bd7b0b78e101b35c9db143fed9c0a
unknown <anuj.k.sharma@hotmail.com>
2022-03-12 11:06:06 +0530

    just checking

---
+++ math.py
@@ -0,0 +1,12 @@
+print(5+3)
+print(2*4)
+print(10-2)
+print(16/2)
+
+favourite = "My lucky number is 8 and I love it because it can be divided into various numbers"
+question = input("what is your lucky number")
+
+print(favourite)
+print("Your lucky number is "+question)
+
+import this
